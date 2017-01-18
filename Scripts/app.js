f73e43e937022660af1b7595184f970aa4779b4d
200359390 <200359390@georgianc.on.ca>
2017-01-18 14:50:26 -0500

    added javaScript click me function

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,2 +1,12 @@
 /*JavaScript Lives here*/ 
 console.log("App Started - Way to keep up!");
+//create a reference to the button on the page (index.html)
+var clickMeButton = document.getElementById("clickMeButton");  
+
+//add an event listener - for the click event and call the Click function
+clickMeButton.addEventListener("click", Click)
+
+//click function - used as an event handler
+function Click() {
+    console.log("clicked!")
+}
